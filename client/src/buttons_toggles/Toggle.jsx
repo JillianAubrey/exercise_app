1a4f54d745d1f5dfabb581adf7fd7d2aeea8c1e0
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-25 22:28:08 -0500

    adding button/toggle components

---
+++ client/src/buttons_toggles/Toggle.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./ToggleButton";
+import ToggleButton from "./ToggleButton";
+import './Toggle.scss'
+
+export default function Toggle (props) {
+  //receives props with a label for each of two buttons and onClick actions for each
+  const { leftLabel, rightLabel, rightClick, leftClick } = props;
+  const [selected, setSelected] = useState("left");
+
+  const onLeftClick = () => {
+    setSelected("left");
+    leftClick();
+  };
+
+  const onRightClick = () => {
+    setSelected("right");
+    rightClick();
+  };
+
+  return (
+    <div class="toggle">
+      <ToggleButton onClick={onLeftClick} selected={selected === "left"}>
+        {leftLabel}
+      </ToggleButton>
+      <ToggleButton onClick={onRightClick} selected={selected === "right"}>
+        {rightLabel}
+      </ToggleButton>
+    </div>
+  );
+}
+
