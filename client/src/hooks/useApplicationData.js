03e2ba703b3b9745f85c66983906aac44f51568d
Michael Davis <michaeltcdavis@gmail.com>
2022-11-26 18:17:00 -0500

    add a hooks folder and a useApplicationData hook

---
+++ client/src/hooks/useApplicationData.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  return {state, }
+}
