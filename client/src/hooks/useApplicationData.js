ef63e05e45589358f3fd5a04b766e7af5ffa27bf
Michael Davis <michaeltcdavis@gmail.com>
2022-11-27 14:01:43 -0500

    begin writing functions to call backend api

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -3,10 +3,36 @@ import axios from 'axios';
 
 export default function useApplicationData() {
   const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
+    //can I store userId in the state in a safe way?
+    user: null,
+    workoutList: []
   });
-  return {state, }
+
+  const setUser = user => setState({ ...state, user });
+
+  useEffect(() => {
+    let ignore = false;
+    getUserWorkouts(state.user) 
+    return () => ignore = true;
+  }, [state.user]);
+  
+  async function getUserWorkouts(userId) { // should this call get all exercise data for workouts that user is a member of?
+    try {
+      const response = await axios.get(`/api/workouts?user=${userId}`);
+      console.log(response);
+      setState((prev) => ({ ...prev, workoutList: response.data }));
+    } catch (error) {
+      console.error("getUserWorkouts error: ", error);
+    }
+  }
+
+  async function saveNewWorkout(workout) {
+
+  }
+
+  async function updateWorkout(workout) {
+
+  }
+
+  return {state, setUser, saveNewWorkout, updateWorkout}
 }
