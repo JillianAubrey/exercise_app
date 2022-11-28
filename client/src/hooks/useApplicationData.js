d9704fe9f1e8bf996fe67557db53b1dc2b952f1d
Michael Davis <michaeltcdavis@gmail.com>
2022-11-27 21:32:14 -0500

    update default state and axios base url

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,24 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3000';
+
 export default function useApplicationData() {
   const [state, setState] = useState({
     //can I store userId in the state in a safe way?
-    user: null,
+    user: 1,
     workoutList: []
   });
 
   const setUser = user => setState({ ...state, user });
 
   useEffect(() => {
-    let ignore = false;
     getUserWorkouts(state.user) 
-    return () => ignore = true;
   }, [state.user]);
   
   async function getUserWorkouts(userId) { // should this call get all exercise data for workouts that user is a member of?
     try {
-      const response = await axios.get(`/api/workouts?user=${userId}`);
+      const response = await axios.get(`/workouts?user=${userId}`);
       console.log(response);
       setState((prev) => ({ ...prev, workoutList: response.data }));
     } catch (error) {
