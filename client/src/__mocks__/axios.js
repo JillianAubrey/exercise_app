24225d95eebaeec4764eb133f855d0664a8bf1f5
Michael Davis <michaeltcdavis@gmail.com>
2022-11-27 21:27:25 -0500

    add mock data

--- client/src/__mocks__/axios.js
+++ client/src/__mocks__/axios.js
@@ -1,10 +1,37 @@
 const fixtures = {
   workouts: {
-
+    "1": {
+      id: 1, exercises: [
+        { exercise_id: 6, reps: 8, sets: 3 },
+        { exercise_id: 7, reps: 8, sets: 3 },
+        { exercise_id: 8, reps: 8, sets: 3 },
+        { exercise_id: 9, reps: 8, sets: 3 },
+        { exercise_id: 10, reps: 8, sets: 3 },
+        { exercise_id: 11, reps: 8, sets: 3 }
+      ]
+    },
+    "2": {
+      id: 1, exercises: [
+        { exercise_id: 6, reps: 8, sets: 3 },
+        { exercise_id: 7, reps: 8, sets: 3 },
+        { exercise_id: 8, reps: 8, sets: 3 },
+        { exercise_id: 9, reps: 8, sets: 3 },
+        { exercise_id: 10, reps: 8, sets: 3 },
+        { exercise_id: 11, reps: 8, sets: 3 }
+      ]
+    }
+  },
+  exercises: {
+    "6": { id: 6, name: 'deadlift', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0032.gif' },
+    "7": { id: 7, name: 'lunge', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0336.gif' },
+    "8": { id: 8, name: 'calf raise', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0417.gif' },
+    "9": { id: 9, name: 'split squat', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0410.gif' },
+    "10": { id: 10, name: 'good morning', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0044.gif' },
+    "11": { id: 11, name: 'squat', category: 'legs', gif_url: 'http://d205bpvrqc9yn1.cloudfront.net/0063.gif' }
   }
-}
+};
 
-export default {
+export default { //do I need to name this object?
   get: jest.fn(url => {
     if (url === "/api/workouts?user=1") {
       return Promise.resolve({
