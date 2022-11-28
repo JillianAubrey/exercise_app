85355ec75b55472e8c3303050d3b5018fc0598d5
Michael Davis <michaeltcdavis@gmail.com>
2022-11-27 21:28:52 -0500

    add test file for application component

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { render, cleanup, waitForElement, fireEvent, getByText, getByPlaceholderText, prettyDOM, getAllByTestId, getByAltText, queryByAltText, getByTestId, queryByText } from "@testing-library/react";
+
+import App from "components/App";
+
+import axios from "axios";
+
+
+describe("App", () => {
+  
+  afterEach(cleanup);
+
+  
+
+
+});
