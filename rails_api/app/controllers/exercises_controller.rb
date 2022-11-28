7f3f03cd32340d7f513bd2df7e11b2662cbc5ab1
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-11-28 05:50:43 +0000

    Add GET route for exercises

---
+++ rails_api/app/controllers/exercises_controller.rb
@@ -0,0 +1,67 @@
+class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show, :update, :destroy]
+
+  # GET /exercises
+  # GET /exercises?user=1
+  # GET /exercises?q=legs
+  # GET /exercises?user=1&q=legs
+  def index
+    if params[:q]
+      where_params = [
+        "name LIKE :q OR category LIKE :q",
+        {q: "%#{params[:q]}%"}
+      ]
+    else
+      where_params = ''
+    end
+
+    if params[:user]
+      @exercises = User.find(params[:user]).exercises.where(where_params)
+    else
+      @exercises = Exercise.db_exercises.where(where_params)
+    end
+
+    render json: @exercises.as_json
+  end
+
+  # GET /exercises/1
+  def show
+    render json: @exercise
+  end
+
+  # POST /exercises
+  def create
+    @exercise = exercise.new(exercise_params)
+
+    if @exercise.save
+      render json: @exercise, status: :created, location: @exercise
+    else
+      render json: @exercise.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /exercises/1
+  def update
+    if @exercise.update(exercise_params)
+      render json: @exercise
+    else
+      render json: @exercise.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /exercises/1
+  def destroy
+    @exercise.destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_exercise
+    @exercise = exercise.find(params[:id])
+  end
+  
+  # Only allow a list of trusted parameters through.
+  def exercise_params
+    params.require(:exercise).permit(:user, :q)
+  end
+end
--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :workouts
+  resources :exercises
   resources :statistics, only: [:show]
 end
