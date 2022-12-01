f52c1176b3faf7ce3d7d60f8b9caf3dfe4196e53
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-30 23:30:33 -0500

    Confirm edit workout is sending requests to the server correctly, add a state for edit in progress

--- client/src/components/WorkoutShow.jsx
+++ client/src/components/WorkoutShow.jsx
@@ -7,28 +7,63 @@ import saveWorkout from "../helpers/saveWorkout";
 export default function WorkoutShow(props) {
   const { exerciseList, user_id } = props;
 
-  const {
-    name,
-    first_gif,
-    ownerName,
-    id: workout_id,
-  } = { ...exerciseList };
+  console.log(props);
+
+  const { name, first_gif, ownerName, id: workout_id } = { ...exerciseList };
 
   const workout_owner = exerciseList.owner.id;
 
-  const exercisesCopy = exerciseList.workout_exercises.map((workout_exercise) => {
-    const exerciseCopy = { ...workout_exercise };
-    exerciseCopy.exercise_id = exerciseCopy.exercise.id
-    delete exerciseCopy.exercise;
-    return exerciseCopy;
-  })
+  console.log("workout exercises:", exerciseList.workout_exercises);
 
-  const [ workoutEdit, setWorkoutEdit ] = useState(exercisesCopy)
+  const exercisesCopy = exerciseList.workout_exercises.map(
+    (workout_exercise) => {
+      const exerciseCopy = { ...workout_exercise };
+      exerciseCopy.exercise_id = exerciseCopy.exercise.id;
+      delete exerciseCopy.exercise;
+      return exerciseCopy;
+    }
+  );
+  console.log("exercisescopy", exercisesCopy);
+
+  const [workoutEdit, setWorkoutEdit] = useState(exercisesCopy);
   const [editMode, setEditMode] = useState(false);
 
-  const handleWorkoutEdit = function(workout_exercise) {
-    console.log(workout_exercise)
+  const handleWorkoutEdit = function (workout_exercise) {
+    setWorkoutEdit((prev) => {
+      delete workout_exercise.name;
+      workout_exercise.id = workout_exercise.workout_exercise_id;
+      delete workout_exercise.workout_exercise_id;
+
+      let newEdit = prev.map((el) => {
+        if (el.id === workout_exercise.id) {
+          return workout_exercise;
+        }
+        return { ...el };
+      });
+
+      if (!workout_exercise.id) {
+        newEdit.push(workout_exercise);
+      }
+
+      console.log("newEdit", newEdit);
+      return newEdit;
+    });
+  };
+
+  const move = (array, from, to) => {
+    array.splice(to, 0, array.splice(from, 1)[0])
   }
+
+  const handleReorder = function (upOrDown, id) {
+    setWorkoutEdit((prev) => {
+      index = prev.findIndex((el) => el.id = id)
+      move(prev, index, upOrDown === 'up' ? index + 1 : index -1)
+      const newEdit = prev.map((el) => ({...el}))
+      console.log(newEdit)
+      return newEdit;
+    })
+  }
+
 
   const exercises = exerciseList.workout_exercises.map((item, index) => {
     const { duration, reps, sets, note, id: workout_exercise_id } = item;
@@ -48,6 +83,7 @@ export default function WorkoutShow(props) {
         note={note}
         editMode={editMode}
         handleWorkoutEdit={handleWorkoutEdit}
+        handleReorder={handleReorder}
         onDelete={() => console.log("hi")}
       />
     );
@@ -62,8 +98,22 @@ export default function WorkoutShow(props) {
         onEdit={() => setEditMode(true)}
       />
       {exercises}
-      {editMode && <Exercise empty={true} handleWorkoutEdit={handleWorkoutEdit} user_id={user_id} />}
-      {editMode && <Toggle leftLabel="Save" rightLabel="Cancel" leftClick={() => saveWorkout({workout_exercises: workoutEdit, name}, workout_id)}/>}
+      {editMode && (
+        <Exercise
+          empty={true}
+          handleWorkoutEdit={handleWorkoutEdit}
+          user_id={user_id}
+        />
+      )}
+      {editMode && (
+        <Toggle
+          leftLabel="Save"
+          rightLabel="Cancel"
+          leftClick={() =>
+            saveWorkout({ workout_exercises: workoutEdit, name }, workout_id)
+          }
+        />
+      )}
     </Fragment>
   );
 }
