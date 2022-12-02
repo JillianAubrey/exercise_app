ef73d7c2413135c679ed1862c928b6b302c3d5b0
Michael Davis <michaeltcdavis@gmail.com>
2022-12-02 11:09:34 -0500

    add member search to add members components

--- client/src/components/Member/MemberListItem.jsx
+++ client/src/components/Member/MemberListItem.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './styles.scss'
 
 export default function MemberListItem(props) {
