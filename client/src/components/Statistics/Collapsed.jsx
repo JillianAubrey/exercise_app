7723eb22f04a36408b452356c45819699a345741
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-12-02 16:46:18 -0500

    Finish functionality of statistics card

---
+++ client/src/components/Statistics/Collapsed.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Collapsed(props) {
+
+  return(
+    <article className="statistics__card-empty" onClick={props.onClick}>View Workout Statistics</article>
+  )
+}
--- client/src/components/Statistics/index.jsx
+++ client/src/components/Statistics/index.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import getStatistics from "../../helpers/api_requests/getStatistics";
 import formatStatistics from "../../helpers/formatStatistics";
+import Collapsed from "./Collapsed";
+import Expanded from "./Expanded";
 
 export default function Statistics(props) {
   const workoutId = props.workoutId;
   const [statistics, setStatistics] = useState(null);
-  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState(null);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpanded = () => {
+    setExpanded(prev => !prev)
+  }
 
   useEffect(() => {
-    getStatistics(workoutId, setError).then((response) => {
+    getStatistics(workoutId, setErrors).then((response) => {
       if (response) {
         const stats = formatStatistics(response)
-        setStatistics(response);
+        setStatistics(stats);
       }
     });
   }, []);
 
-  return <h1>hello</h1>;
+  return (<article className="statistics__container">
+            {expanded && <Expanded statistics={statistics} onClick={handleExpanded} error={errors}/>}
+            {!expanded && <Collapsed onClick={handleExpanded}/>}
+        </article>)
 }
