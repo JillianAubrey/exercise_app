46ba0b08852c3cc523ba3eec45c50eb7a36c878a
Michael Davis <michaeltcdavis@gmail.com>
2022-12-05 18:16:52 -0500

    modify database and seeds to contain/validate downcased emails

--- rails_api/app/models/user.rb
+++ rails_api/app/models/user.rb
@@ -4,9 +4,17 @@ class User < ApplicationRecord
   has_many :exercises, :dependent => :destroy
   has_many :walkthroughs, :dependent => :destroy
 
+  before_validation :downcase_email
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
