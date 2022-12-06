9049985b41389a1c1ba3b268bf348b3270a1d245
Michael Davis <michaeltcdavis@gmail.com>
2022-12-05 19:29:14 -0500

    fix member component ajax call order

--- client/src/helpers/api_requests/getMembers.js
+++ client/src/helpers/api_requests/getMembers.js
@@ -2,10 +2,8 @@ import api from "./_api";
 
 export default async function getMembers(workoutId, onSuccess, onError) { 
   try {
-    console.log('getting members...')
     const response = await api.get(`/workouts/${workoutId}/members`)
-    console.log('onSuccessing data...')
-    onSuccess && onSuccess(response.data);
+    return response.data;
   } catch (error) {
     onError && onError(error);
   }
