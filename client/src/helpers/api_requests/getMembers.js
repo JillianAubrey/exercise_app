2611b212cd0598e50bfe6ca072de382009b93830
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-12-06 00:42:45 +0000

    Resolve merge conflicts

--- client/src/helpers/api_requests/getMembers.js
+++ client/src/helpers/api_requests/getMembers.js
@@ -3,11 +3,7 @@ import api from "./_api";
 export default async function getMembers(workoutId, onSuccess, onError) { 
   try {
     const response = await api.get(`/workouts/${workoutId}/members`)
-<<<<<<< HEAD
-    onSuccess && onSuccess(response.data);
-=======
     return response.data;
->>>>>>> 9049985b41389a1c1ba3b268bf348b3270a1d245
   } catch (error) {
     onError && onError(error);
   }
